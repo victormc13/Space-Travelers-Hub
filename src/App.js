b58d65f02dee5cd1010a8d0f2ed3091f074fac01
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-08-28 20:48:59 +0100

    Added route, view, and NavLink for page navigation with styled active class to indicate current section/page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation/Navigation';
+import Rockets from './components/Rockets/Rockets';
+import Missions from './components/Missions/Missions';
+import Dragons from './components/Dragons/Dragons';
+import Profile from './components/Profile/Profile';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Space Travelers Hub</h1>
+const App = () => (
+  <>
+    <Navigation />
+    <div className="container">
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/dragons" element={<Dragons />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
     </div>
-  );
-}
-
+  </>
+);
 export default App;
