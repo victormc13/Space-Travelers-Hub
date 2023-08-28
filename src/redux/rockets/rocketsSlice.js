cff12d5aa1bc9aa01768b0fd68476f7989e79fda
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-08-29 00:26:40 +0100

    Add rocket data fetching functionality

---
+++ src/redux/rockets/rocketsSlice.js
@@ -0,0 +1,40 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchRockets = createAsyncThunk('rockets/fetchRockets', async () => {
+  try {
+    const response = await axios.get('https://api.spacexdata.com/v4/rockets');
+    return response.data;
+  } catch (error) {
+    throw Error('Failed to fetch rocket data');
+  }
+});
+
+const initialState = {
+  rockets: [],
+  status: 'idle',
+  error: null,
+};
+
+const rocketSlice = createSlice({
+  name: 'rockets',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRockets.pending, (state) => {
+        state.status = 'loading';
+        state.error = null;
+      })
+      .addCase(fetchRockets.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.rockets = action.payload;
+      })
+      .addCase(fetchRockets.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default rocketSlice.reducer;
