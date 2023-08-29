a95cff01a0b75da96f3ad8266603da7565effe08
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-08-29 17:24:49 +0100

    Streamlined rendering logic in Rockets component using conditional operators.

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -14,27 +14,20 @@ function Rockets() {
     }
   }, [status, dispatch]);
 
-  let content;
-
-  if (status === 'loading') {
-    content = <div>Loading...</div>;
-  } else if (status === 'failed') {
-    content = <div>Error: Something went wrong</div>;
-  } else {
-    content = (
-      <div className="rockets-container">
-        {rockets.map((rocket) => (
+  return (
+    <div className="rockets-container">
+      {status === 'loading' && <div>Loading...</div>}
+      {status === 'failed' && <div>Error: Something went wrong</div>}
+      {status === 'succeeded'
+        && rockets.map((rocket) => (
           <Rocket
             key={rocket.id}
             rocket={rocket}
             onReserve={(rocketId) => dispatch(reserve(rocketId))}
           />
         ))}
-      </div>
-    );
-  }
-
-  return content;
+    </div>
+  );
 }
 
 export default Rockets;
