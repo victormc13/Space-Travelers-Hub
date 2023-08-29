a44cdd2738f1a8fde8400ab8d4fcf4c4e47f8bcc
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-08-29 13:58:40 +0100

    Display rockets Lists render

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import './Rockets.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets, reserve } from '../../redux/rockets/rocketsSlice';
+import Rocket from './Rocket';
 
 function Rockets() {
-  return (
-    <div>Rockets</div>
-  );
+  const dispatch = useDispatch();
+  const { rockets, status } = useSelector((state) => state.rockets);
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchRockets());
+    }
+  }, [status, dispatch]);
+
+  let content;
+
+  if (status === 'loading') {
+    content = <div>Loading...</div>;
+  } else if (status === 'failed') {
+    content = <div>Error: Something went wrong</div>;
+  } else {
+    content = (
+      <div className="rockets-container">
+        {rockets.map((rocket) => (
+          <Rocket
+            key={rocket.id}
+            rocket={rocket}
+            onReserve={(rocketId) => dispatch(reserve(rocketId))}
+          />
+        ))}
+      </div>
+    );
+  }
+
+  return content;
 }
 
 export default Rockets;
