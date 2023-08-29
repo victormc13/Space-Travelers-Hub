45597c5cb3f52819ae32ffdcb481a2d3a4bfd7c2
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-08-29 14:03:43 +0100

    Display reserved rockets in My profile - Filter

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import './Profile.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../../redux/rockets/rocketsSlice';
 
-const Profile = () => (
-  <div>Profile</div>
-);
+function Profile() {
+  const dispatch = useDispatch();
+  const { rockets, status } = useSelector((state) => state.rockets);
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchRockets());
+    }
+  }, [status, dispatch]);
+
+  return (
+    <>
+      <h2>My Rockets</h2>
+      <table>
+        <tbody>
+          {rockets.map((rocket) => (
+            rocket.reserved && (
+            <tr key={rocket.id}>
+              <td>{rocket.name}</td>
+            </tr>
+            )
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
 
 export default Profile;
