918f640ad022d606e376ee1715cf40ab9f6d3847
victormc13 <viktorsb14@gmail.com>
2023-08-29 23:47:03 -0500

    Add missions reserved for the user and show up in profile section

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import './Profile.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchRockets } from '../../redux/rockets/rocketsSlice';
@@ -6,6 +6,7 @@ import { fetchRockets } from '../../redux/rockets/rocketsSlice';
 function Profile() {
   const dispatch = useDispatch();
   const { rockets, status } = useSelector((state) => state.rockets);
+  const { missions } = useSelector((state) => state.missions);
 
   useEffect(() => {
     if (status === 'idle') {
@@ -14,20 +15,34 @@ function Profile() {
   }, [status, dispatch]);
 
   return (
-    <>
-      <h2>My Rockets</h2>
-      <table>
-        <tbody>
-          {rockets.map((rocket) => (
-            rocket.reserved && (
-            <tr key={rocket.id}>
-              <td>{rocket.name}</td>
-            </tr>
-            )
-          ))}
-        </tbody>
-      </table>
-    </>
+    <div className="profile-section">
+      <div className="missions-section">
+        <h2>My Missions</h2>
+        <table>
+          <tbody>
+            {missions.filter((mission) => mission.reserved).map((mission) => (
+              <tr key={mission.mission_id}>
+                <td>{mission.mission_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="rockets-section">
+        <h2>My Rockets</h2>
+        <table>
+          <tbody>
+            {rockets.map((rocket) => (
+              rocket.reserved && (
+                <tr key={rocket.id}>
+                  <td>{rocket.name}</td>
+                </tr>
+              )
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
 
